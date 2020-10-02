f5181ba802569fe5eee2866014ac29a1d608a329
Michael Shevelin <shevel55505@gmail.com>
2020-10-02 17:55:06 +0300

    wind list done

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
-from selenium import webdriver
+from tkinter import *
+import time
 import requests
+
 
 
 html_string = requests.get("https://magicseaweed.com/Backdoor-Haifa-Surf-Report/3987/").content
@@ -18,18 +20,33 @@ for a in soup.findAll('span', attrs={'class':'h3 font-sans-serif heavy nomargin 
     else:
         higher_weekly.append(a.contents[2])
 
-reports = []
+
 reports_len = len(higher_weekly)
 for a in soup.findAll('ul', attrs={'class':'rating clearfix'}):
     ais.append(a)
-print(reports_len)
-print(len(ais))
+
+
 stars = []
-for k in ais:
+for k in soup.find_all('td'):
     stars.append(str(k).count('active'))
-print(stars)
 
 
+conditions = [ ]
+winds = []
+for tr in soup.find_all('tr'):
+    for td in tr.find_all('td'):
+        if type(td.get('title')) == str:
+            conditions.append(td.get('title'))
 
-# for i in range(reports_len):
-#     for j in ais[i]:
+
+for i in conditions:
+    if i.startswith('Light') or i.startswith('Very') or i.startswith('Moderate') or i.startswith('Gentle') or i.startswith('Fresh'):
+        winds.append(i)
+
+
+t = time.localtime()
+current_time = time.strftime("%H:%M:%S", t)
+
+#GUI CODE STARDS HERE:
+root = Tk()
+frame = LabelFrame()
